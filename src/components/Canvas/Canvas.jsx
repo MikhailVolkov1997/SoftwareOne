21c9b8e40128c13ef6d465ab9a89a0742efd783e
Mikhail.Volkov <mikhail.volkov.135@gmail.com>
2021-02-10 18:51:34 +0200

    created canvas

---
+++ src/components/Canvas/Canvas.jsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+import DiagramWrapper from './DiagramWrapper'
+
+export default class Canvas extends React.Component {
+  render() {
+    return <DiagramWrapper />
+  }
+}
--- src/utils/usePopulateDetails.js
+++ src/utils/usePopulateDetails.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { EntityService } from '../services'
 
 const getAttributes = (entity) => {
