f7aa1291802b3a108569c6e3a29ea8d39a48deb0
dpcarnage <mikhail.volkov.135@gmail.com>
2021-02-11 14:54:29 +0200

    created diagram modal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css'
+import Canvas from './components/Canvas/Canvas'
 import EntityList from './components/EntityList/EntityList'
 import { Timerange } from './components/Timerange/Timerange'
 
 function App() {
   return (
     <div className="App">
-      <Timerange />
+      <Canvas />
     </div>
   )
 }
--- src/components/Canvas/DiagramWrapper.jsx
+++ src/components/Canvas/DiagramWrapper.jsx
@@ -6,139 +6,100 @@ import { FreehandDrawingTool } from './FreehandDrawingTool'
 import { GeometryReshapingTool } from './GeometryReshapingTool'
 
 import './Canvas.css'
+import diagramOptions from './diagram.options'
+import Modal from '../Modal'
 
 export default class DiagramWrapper extends React.Component {
-  /**
-   * Ref to keep a reference to the Diagram component, which provides access to the GoJS diagram via getDiagram().
-   */
-
-  /** @internal */
   constructor(props) {
     super(props)
     this.diagramRef = React.createRef()
+
+    this.state = {
+      openModal: false,
+      modalData: null
+    }
   }
 
-  /**
-   * Get the diagram reference and add any desired diagram listeners.
-   * Typically the same function will be used for each listener, with the function using a switch statement to handle the events.
-   */
+  onEventListener = (event) => {
+    const {
+      subject: {
+        part: { data }
+      }
+    } = event
+
+    this.setState({ openModal: true, modalData: data })
+  }
+
+  closeModal = () => {
+    this.setState({ openModal: false })
+  }
+
   componentDidMount() {
     if (!this.diagramRef.current) return
     const diagram = this.diagramRef.current.getDiagram()
     if (diagram instanceof go.Diagram) {
-      diagram.addDiagramListener('ChangedSelection', this.props.onDiagramEvent)
+      diagram.addDiagramListener('ObjectSingleClicked', this.onEventListener)
     }
   }
 
-  /**
-   * Get the diagram reference and remove listeners that were added during mounting.
-   */
   componentWillUnmount() {
     if (!this.diagramRef.current) return
     const diagram = this.diagramRef.current.getDiagram()
     if (diagram instanceof go.Diagram) {
-      diagram.removeDiagramListener(
-        'ChangedSelection',
-        this.props.onDiagramEvent
-      )
+      diagram.removeDiagramListener('ChangedSelection', this.onEventListener)
     }
   }
 
-  /**
-   * Diagram initialization method, which is passed to the ReactDiagram component.
-   * This method is responsible for making the diagram and initializing the model, any templates,
-   * and maybe doing other initialization tasks like customizing tools.
-   * The model's data should not be set here, as the ReactDiagram component handles that.
-   */
-  initDiagram() {
+  initDiagram = () => {
     const $ = go.GraphObject.make
     // set your license key here before creating the diagram: go.Diagram.licenseKey = "...";
     const diagram = $(go.Diagram, {
-      'undoManager.isEnabled': true, // must be set to allow for model change listening
-      // 'undoManager.maxHistoryLength': 0,  // uncomment disable undo/redo functionality
-      'clickCreatingTool.archetypeNodeData': {
-        text: 'new node',
-        color: 'lightblue'
-      },
-      //   draggingTool: new FreehandDrawingTool(), // defined in GuidedDraggingTool.ts
-      layout: $(go.ForceDirectedLayout),
-      model: $(go.GraphLinksModel, {
-        linkKeyProperty: 'key', // IMPORTANT! must be defined for merges and data sync when using GraphLinksModel
-        // positive keys for nodes
-        makeUniqueKeyFunction: (m, data) => {
-          let k = data.key || 1
-          while (m.findNodeDataForKey(k)) k++
-          data.key = k
-          return k
-        },
-        // negative keys for links
-        makeUniqueLinkKeyFunction: (m, data) => {
-          let k = data.key || -1
-          while (m.findLinkDataForKey(k)) k--
-          data.key = k
-          return k
-        }
-      })
+      allowCopy: false,
+      allowDelete: false,
+      allowMove: false,
+      initialAutoScale: go.Diagram.Uniform,
+      layout: $('TreeLayout', {
+        angle: 90,
+        compaction: go.TreeLayout.CompactionNone
+      }),
+      'undoManager.isEnabled': true
     })
 
-    diagram.toolManager.mouseDownTools.insertAt(3, new GeometryReshapingTool())
-
-    diagram.nodeTemplateMap.add(
-      'FreehandDrawing',
+    diagram.nodeTemplate = $(
+      go.Node,
+      'Vertical',
+      { selectionObjectName: 'BODY' },
       $(
-        go.Part,
-        { locationSpot: go.Spot.Center, isLayoutPositioned: false },
-        new go.Binding('location', 'loc', go.Point.parse).makeTwoWay(
-          go.Point.stringify
-        ),
-        {
-          selectionAdorned: true,
-          selectionObjectName: 'SHAPE',
-          // custom selection adornment: a blue rectangle
-          selectionAdornmentTemplate: $(
-            go.Adornment,
-            'Auto',
-            $(go.Shape, { stroke: 'dodgerblue', fill: null }),
-            $(go.Placeholder, { margin: -1 })
-          )
-        },
-        { resizable: true, resizeObjectName: 'SHAPE' },
-        { rotatable: true, rotateObjectName: 'SHAPE' },
-        { reshapable: true }, // GeometryReshapingTool assumes nonexistent Part.reshapeObjectName would be "SHAPE"
+        go.Panel,
+        'Auto',
+        { name: 'BODY' },
         $(
           go.Shape,
-          { name: 'SHAPE', fill: null, strokeWidth: 1.5 },
-          new go.Binding('desiredSize', 'size', go.Size.parse).makeTwoWay(
-            go.Size.stringify
-          ),
-          new go.Binding('angle').makeTwoWay(),
-          new go.Binding('geometryString', 'geo').makeTwoWay(),
+          'RoundedRectangle',
           new go.Binding('fill'),
-          new go.Binding('stroke'),
-          new go.Binding('strokeWidth')
+          new go.Binding('stroke')
+        ),
+        $(
+          go.TextBlock,
+          {
+            font: 'bold 12pt Arial, sans-serif',
+            margin: new go.Margin(4, 2, 2, 2)
+          },
+          new go.Binding('text')
         )
+      ),
+      $(
+        go.Panel, // this is underneath the "BODY"
+        { height: 17 }, // always this height, even if the TreeExpanderButton is not visible
+        $('TreeExpanderButton')
       )
     )
 
-    const tool = new FreehandDrawingTool()
-    // provide the default JavaScript object for a new polygon in the model
-    tool.archetypePartData = {
-      stroke: 'black',
-      strokeWidth: 1.5,
-      category: 'FreehandDrawing'
-    }
-    // allow the tool to start on top of an existing Part
-    tool.isBackgroundOnly = false
-    // install as first mouse-move-tool
-    diagram.toolManager.mouseMoveTools.insertAt(0, tool)
+    diagram.linkTemplate = $(go.Link, $(go.Shape, { strokeWidth: 1.5 }))
+
+    diagram.model = $(go.TreeModel, { nodeDataArray: diagramOptions })
 
     return diagram
-  }
-
-  modeChange = (isDraw) => {
-    const diagram = this.diagramRef.current.getDiagram()
-    const tool = diagram.toolManager.findTool('FreehandDrawing')
-    tool.isEnabled = isDraw
   }
 
   render() {
@@ -146,13 +107,15 @@ export default class DiagramWrapper extends React.Component {
       <div className="diagram-wrapper">
         <ReactDiagram
           ref={this.diagramRef}
-          divClassName="diagram-component"
           initDiagram={this.initDiagram}
+          divClassName="diagram-component"
+          onModelChange={(e) => console.log(e)}
         />
-        <div className="mode-buttons">
-          <button onClick={() => this.modeChange(false)}>Select</button>
-          <button onClick={() => this.modeChange(true)}>Draw mode</button>
-        </div>
+        <Modal
+          modalData={this.state.modalData}
+          open={this.state.openModal}
+          closeModal={this.closeModal}
+        />
       </div>
     )
   }
